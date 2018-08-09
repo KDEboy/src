224d08b39b680e2c231df3668d9378032283a7b2
KDEboy <codecodic7@gmail.com>
2018-08-09 08:49:36 -0300

    Em fase de pré planejamento ainda...

---
+++ Akinator.c
@@ -0,0 +1,20 @@
+/*Akinator para Noobs -> Este projeto garante que funciona no meu PC, ja no teu não faço ideia, ou seja, não há garantia nenhuma.
+
+ATUALMENTE ISSO TÁ EM BETA*/
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main(void){
+
+
+
+
+
+
+	return 0;
+}
